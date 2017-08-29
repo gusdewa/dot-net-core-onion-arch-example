d783ba82b8f2b0911edd2e45f19c481f8fc89cf9
Rahadian Dewandono <gusdewa@gmail.com>
2017-08-30 00:32:11 +0800

    F-004 - Set Console App will not Exit except Typed Exit or Ctrl C

--- ParkingLot.ConsoleApp/Program.cs
+++ ParkingLot.ConsoleApp/Program.cs
@@ -14,44 +14,47 @@ namespace ParkingLot.ConsoleApp
             string input;
             do
             {
-                input = Console.ReadLine();
-            } while (!input.IsStartProgramStatements() && !input.IsBreakProgramStatement());
-
-            // Break the program immediately
-            if (input.IsBreakProgramStatement()) return;
-
-            // Instantiate app service and dependencies
-            ConsoleWriter consoleWriter = new ConsoleWriter();
-            IParkingLotCommandService service = new ParkingLotCommandService(consoleWriter);
-
-            if (input.ReadFromFileStatement())
-            {
-                TextFileReader textFileReader = new TextFileReader(consoleWriter);
-                StringBuilder content = textFileReader.ReadAsString(input.GetFileName());
-
-                // Extract each line into list of command statements
-                IEnumerable<KeyValuePair<string, string[]>> commandStatements =
-                    service.ExtractCommandStatements(content.ToString());
-
-                // Register the list of commands at once
-                service.RegisterAll(commandStatements);
-
-                // Exceute based on the order of the queue
-                service.ExecuteAll();
-            }
-            else
-            {
                 do
                 {
                     input = Console.ReadLine();
-                    if (input.IsBreakProgramStatement()) break;
+                } while (!input.IsStartProgramStatements() && !input.IsBreakProgramStatement());
 
-                    // Register the command
-                    service.Register(input.GetCommandName(), input.GetArguments());
-                    // Execute the command. Command queue is always 0 or 1.
-                    service.Execute();
-                } while (!input.IsBreakProgramStatement());
-            }
+                // Break the program immediately
+                if (input.IsBreakProgramStatement()) return;
+
+                // Instantiate app service and dependencies
+                ConsoleWriter consoleWriter = new ConsoleWriter();
+                IParkingLotCommandService service = new ParkingLotCommandService(consoleWriter);
+
+                if (input.ReadFromFileStatement())
+                {
+                    TextFileReader textFileReader = new TextFileReader(consoleWriter);
+                    StringBuilder content = textFileReader.ReadAsString(input.GetFileName());
+
+                    // Extract each line into list of command statements
+                    IEnumerable<KeyValuePair<string, string[]>> commandStatements =
+                        service.ExtractCommandStatements(content.ToString());
+
+                    // Register the list of commands at once
+                    service.RegisterAll(commandStatements);
+
+                    // Exceute based on the order of the queue
+                    service.ExecuteAll();
+                }
+                else
+                {
+                    do
+                    {
+                        input = Console.ReadLine();
+                        if (input.IsBreakProgramStatement()) return;
+
+                        // Register the command
+                        service.Register(input.GetCommandName(), input.GetArguments());
+                        // Execute the command. Command queue is always 0 or 1.
+                        service.Execute();
+                    } while (!input.IsBreakProgramStatement());
+                }
+            } while (!input.IsBreakProgramStatement());
         }
     }
 }
